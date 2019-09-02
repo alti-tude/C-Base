f20b98615ed343ad7666667087edc2a9e0eb9ef7
alti-tude <kartik.gupta0204@gmail.com>
2019-09-02 12:27:08 +0530

    first commit

---
+++ main.h
@@ -0,0 +1,34 @@
+#ifndef MAIN_H
+#define MAIN_H
+#define NULL_VAL -1000000
+#include<bits/stdc++.h>
+
+
+class TableObj{
+private:
+    std::map<std::string, int> col_map;
+    int num_cols;
+    int num_rows;
+public:
+//Every vector stores one column. To add a col, add a new vector, to add a record, add one to the end of every vector.
+    std::vector<std::vector<int> > cols;
+
+    TableObj():num_cols(0),num_rows(0){}
+    ~TableObj();
+
+    TableObj getByColumns(std::vector<std::string> cols);
+    void addColumn(std::string col_name, std::vector<int> col_data);
+    void addRow(std::vector<int> row);
+};
+
+class Database{
+private: 
+    
+public:
+    Database();
+    ~Database();
+
+    TableObj getTable(std::string name);
+};
+
+#endif
